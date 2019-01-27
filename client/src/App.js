66e9e772fa003c55f687cc755a87cdd57c44e617
Stephen Joel <stephen.janagarajan@ttu.edu>
2019-01-27 02:58:29 -0600

    Combined back-end

--- client/src/App.js
+++ client/src/App.js
@@ -1,57 +1,96 @@
-import React, { Component } from 'react';
-import './App.css';
-import SimpleMap from './components/SimpleMap';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
-import SimpleCard from './components/SimpleCard';
+import React, { Component } from "react";
+import "./App.css";
+import SimpleMap from "./components/SimpleMap";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import SimpleCard from "./components/SimpleCard";
+import axios from "axios";
 
 class App extends Component {
   constructor() {
-    super()
+    super();
     this.state = {
-      data: []
-    }
+      data: [],
+      currentLat: 30.628,
+      currentLng: -96.3344
+    };
   }
 
   componentDidMount() {
-    // fetch('/airports?{}')
-    //   .then(res => {
-    //       console.log(res);
-    //       return res.json()
-    //    })
-    //   .then(users => { 
-    //       console.log(users); 
-    //       this.setState({ users })
-    //    });
-   }
+    if (navigator && navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(pos => {
+        const coords = pos.coords;
+        console.log(coords.latitude, coords.longitude);
+        this.setState({
+          data: [],
+          lat: coords.latitude,
+          lng: coords.longitude
+        });
+      });
+    }
+
+    axios
+      .get("https://localhost:3005/airports/34/67/78/23")
+      .then(res => {
+        console.log(res);
+      })
+      .then(users => {
+        console.log(users);
+        this.setState({ users });
+      });
+  }
+
+  handlePost = form => {
+    console.log(form);
+  };
 
   render() {
-    
     return (
       <div>
-        <AppBar position="static" color='primary'>
-        <Toolbar>
-          <Typography variant="h6" color="inherit">
-            TAMUHack2019
-          </Typography>
-        </Toolbar>
+        <AppBar position="static" color="primary">
+          <Toolbar>
+            <Typography variant="h6" color="inherit">
+              TAMUHack2019
+            </Typography>
+          </Toolbar>
         </AppBar>
-        <div style={{display: "flex"}}>
-          <div style={{height: '100%', width: '60%', padding: '10px', backgroundColor: '#FF7D93', display: 'flex', flexDirection: 'column'}}>
-            <SimpleMap content = {this.state.data}/>
+        <div style={{ display: "flex" }}>
+          <div
+            style={{
+              height: "100%",
+              width: "60%",
+              padding: "10px",
+              backgroundColor: "#FF7D93",
+              display: "flex",
+              flexDirection: "column"
+            }}
+          >
+            <SimpleMap
+              handlePost={this.handlePost}
+              lat={this.state.currentLat}
+              lng={this.state.currentLng}
+              content={this.state.data}
+            />
           </div>
-          <div style={{height: '860px', overflow: 'auto', width: '40%', padding: '10px', backgroundColor: '#FF7D93'}}>
-            {this.state.data.map(function(object, i){
-                let temp = [];
-                temp.push(<SimpleCard content = {object} key={i}/>);
-                temp.push(<br/>);
-                return temp;
+          <div
+            style={{
+              height: "860px",
+              overflow: "auto",
+              width: "40%",
+              padding: "10px",
+              backgroundColor: "#FF7D93"
+            }}
+          >
+            {this.state.data.map(function(object, i) {
+              let temp = [];
+              temp.push(<SimpleCard content={object} key={i} />);
+              temp.push(<br />);
+              return temp;
             })}
           </div>
         </div>
       </div>
-
     );
   }
 }
--- client/src/components/SimpleMap.js
+++ client/src/components/SimpleMap.js
@@ -1,71 +1,78 @@
-import React, {Component} from 'react';
+import React, { Component } from "react";
 import { compose, withStateHandlers } from "recompose";
-import { InfoWindow, withGoogleMap, withScriptjs, GoogleMap, Marker } from 'react-google-maps';
-
+import {
+  InfoWindow,
+  withGoogleMap,
+  withScriptjs,
+  GoogleMap,
+  Marker
+} from "react-google-maps";
 
 const Map = compose(
-    withStateHandlers(() => ({
-        isMarkerShown: false,
-        markerPosition: null
-      }), {
-        onMapClick: ({ isMarkerShown }) => (e) => ({
-            markerPosition: e.latLng,
-            isMarkerShown:true,
-        })
-      }),
-    withScriptjs,
-    withGoogleMap
-)
-    (props =>
-        <GoogleMap
-            defaultZoom={8}
-            defaultCenter={{ lat: props.lat, lng: props.lng}}
-            onClick={props.onMapClick}
-        >
-            {props.isMarkerShown && <Marker position={props.markerPosition} icon = 'https://i.ibb.co/DMJR4WJ/map-marker-hi-2-1.png'/>}
-            { <Marker position = {{ lat: props.lat, lng: props.lng }} icon ='https://i.ibb.co/PNLnTpX/map-marker-hi-2.png'/>}
-            {props.data.map(function(object, i){
-                return <Marker position = {{ lat: object.lat, lng: object.lng }} icon ='https://i.ibb.co/PNLnTpX/map-marker-hi-2.png'/>
-            })}
-        </GoogleMap>
-    )
-
-export default class SampleMap extends React.Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-          lat: 30.6280,
-          lng: -96.3344
-        }
-    }
-    
-    componentDidUpdate() {
-      if (navigator && navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(pos => {
-          const coords = pos.coords;
-          console.log(coords.latitude, coords.longitude)
-          this.setState({
-              lat: coords.latitude,
-              lng: coords.longitude
-          });
-        });
+  withStateHandlers(
+    () => ({
+      isMarkerShown: false,
+      markerPosition: null
+    }),
+    {
+      onMapClick: ({ isMarkerShown }) => e => {
+        return {
+          markerPosition: e.latLng,
+          isMarkerShown: true
+        };
       }
     }
-
-    render() {
-
-        return (
-            <div style={{ height: '100%' }}>
-                <Map
-                    googleMapURL="https://maps.googleapis.com/maps/api/js?key=AIzaSyCWM4QgKRRQIin27qATBf463PHhzA4kaug"
-                    loadingElement={<div style={{ height: `100%` }} />}
-                    containerElement={<div style={{ height: `860px` }}/>}
-                    mapElement={<div style={{ height: `100%` }} />}
-                    lat={this.state.lat}
-                    lng={this.state.lng}
-                    data = {this.props.content}
-                />
-            </div>
-        )
+  ),
+  withScriptjs,
+  withGoogleMap
+)(props => (
+  <GoogleMap
+    defaultZoom={8}
+    defaultCenter={{ lat: props.lat, lng: props.lng }}
+    onClick={props.onMapClick}
+  >
+    {props.isMarkerShown && (
+      <Marker
+        position={props.markerPosition}
+        icon="https://i.ibb.co/DMJR4WJ/map-marker-hi-2-1.png"
+      />
+    )}
+    {
+      <Marker
+        position={{ lat: props.lat, lng: props.lng }}
+        icon="https://i.ibb.co/PNLnTpX/map-marker-hi-2.png"
+      />
     }
+    {props.data.map(function(object, i) {
+      return (
+        <Marker
+          position={{ lat: object.lat, lng: object.lng }}
+          icon="https://i.ibb.co/PNLnTpX/map-marker-hi-2.png"
+        />
+      );
+    })}
+  </GoogleMap>
+));
+
+export default class SampleMap extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div style={{ height: "100%" }}>
+        <Map
+          googleMapURL="https://maps.googleapis.com/maps/api/js?key=AIzaSyCWM4QgKRRQIin27qATBf463PHhzA4kaug"
+          loadingElement={<div style={{ height: `100%` }} />}
+          containerElement={<div style={{ height: `860px` }} />}
+          mapElement={<div style={{ height: `100%` }} />}
+          lat={this.props.lat}
+          lng={this.props.lng}
+          data={this.props.content}
+          handlePost={this.props.handlePost}
+        />
+      </div>
+    );
+  }
 }
